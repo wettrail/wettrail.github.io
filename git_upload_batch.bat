cb5f252f9126978789ca6eaf47a2a5f987c00b37
Kyle <135763545+wettrail@users.noreply.github.com>
2023-06-09 06:10:11 -0400

    batch_commit _ table update

--- git_upload_batch.bat
+++ git_upload_batch.bat
@@ -1,41 +1,17 @@
 @echo off
 echo Pushing local repository to GitHub...
-echo.
-"C:\Program Files\Git\git-bash.exe" --cd=C:\Users\Kyle\Documents\database_geog655\FME_Trail_Rain\Web\wettrail.github.io  git commit -m 'this is my first command commit - table updates' 
-setlocal
+@echo off
+"C:\Program Files\Git\git-bash.exe" 
+@echo off
 
-rem Set your local repository path
-set "localRepoPath=C:\Users\Kyle\Documents\database_geog655\FME_Trail_Rain\Web\wettrail.github.io"
+REM Change the directory to your Git repository
+cd "C:\Users\Kyle\Documents\database_geog655\FME_Trail_Rain\Web\wettrail.github.io"
 
-rem Set your GitHub repository URL
-set "githubRepoURL=https://github.com/wettrail/wettrail.github.io.git"
-
-rem Set your GitHub username and access token (or password)
-set "githubUsername=wettrail"
-set "githubAccessToken=flr6washGH"
-
-rem Change to the local repository directory
-cd /d "%localRepoPath%"
-
-rem Initialize Git repository if not already done
-if not exist ".git" (
-    git init
-)
-
-rem Add all files to the Git repository
+REM Add the files to the staging area
 git add .
 
-rem Commit the changes
-git commit -m "updated tables from batch"
+REM Commit the changes with a custom message
+git commit -m "batch_commit _ table update"
 
-rem Set the GitHub repository as the remote origin
-git remote add origin %githubRepoURL%
-
-rem Push the local repository to the GitHub repository
-git push -u main
-
-endlocal
-
-echo.
-echo Local repository pushed to GitHub successfully.
-pause
+REM Push the changes to the remote repository
+git push origin main
